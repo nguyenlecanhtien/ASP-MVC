1b2ac1bf793b02bf7f429603474db005d1751ec3
nguyenlecanhtien <126738476+nguyenlecanhtien@users.noreply.github.com>
2023-03-02 09:46:41 +0700

    first commit

---
+++ WebSiteBanHang_MVC_2120110131/WebSiteBanHang_MVC_2120110131/Areas/Author/AuthorAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebSiteBanHang_MVC_2120110131.Areas.Author
+{
+    public class AuthorAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Author";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Author_default",
+                "Author/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebSiteBanHang_MVC_2120110131/WebSiteBanHang_MVC_2120110131/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebSiteBanHang_MVC_2120110131.MvcApplication" Language="C#" %>
